f92b0776e78a5f1de58fd305e88e42adddb48568
yasuaki640 <yasuaki640@gmail.com>
2022-06-30 10:18:24 +0900

    wip

---
+++ config/dotenv/e2e-test-config.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+console.log('test config called');
+
+// できれば環境変数を上書きするオプションがほしいところ
+// https://github.com/motdotla/dotenv/issues/517
+const testEnv = dotenv.config({
+  path: path.join(process.cwd(), '.env.testing'),
+});
+
+// 明示的に環境変数を上書き
+Object.assign(process.env, {
+  ...testEnv.parsed,
+});
