02fdd311a7a72773d97fcecdf21e43936d36b981
yasuaki640 <yasuaki640@gmail.com>
2022-06-30 17:55:34 +0900

    a;siejfale

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -6,6 +6,7 @@ import { User } from '../src/users/entities/user.entity';
 import { UsersModule } from '../src/users/users.module';
 import { generateCreateUserDto } from './faker/users-faker';
 import { ErrorResponse } from './type/http';
+import { UpdateUserDto } from '../src/users/dto/update-user.dto';
 
 describe('UserController (e2e)', () => {
   let app: INestApplication;
@@ -80,6 +81,40 @@ describe('UserController (e2e)', () => {
     expect(getAllRes.body[0]).toEqual(expected[0]);
     expect(getAllRes.body[1]).toEqual(expected[1]);
     expect(getAllRes.body[2]).toEqual(expected[2]);
+  });
+
+  it('can update specific user', async () => {
+    const created = await postUser(app);
+
+    const updateData: UpdateUserDto = {
+      firstName: 'modified first name',
+      lastName: 'modified last name',
+      isActive: false,
+    };
+
+    const putRes: { body: User } = await request(app.getHttpServer())
+      .put(`/users/${created.id}`)
+      .send(updateData)
+      .expect(200);
+
+    expect(putRes.body).toEqual(expect.objectContaining(updateData));
+  });
+
+  it('should return 404 if specified update target id does not exist.', async () => {
+    const updateData: UpdateUserDto = {
+      firstName: 'modified first name',
+      lastName: 'modified last name',
+      isActive: false,
+    };
+
+    const putRes: { body: ErrorResponse } = await request(app.getHttpServer())
+      .put('/users/9999999999')
+      .send(updateData)
+      .expect(404);
+
+    expect(putRes.body.error).toBe('Not Found');
+    expect(putRes.body.message).toBe("Specified user doesn't exists");
+    expect(putRes.body.statusCode).toBe(404);
   });
 
   // MEMO CloseしないとJestが終了しない
